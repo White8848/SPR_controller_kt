87497f30ae4b5d1ba55bc5a1c479f2a4c88780ef
Yirule <2356758998@qq.com>
2022-10-20 18:58:00 +0800

    添加了usb手柄数据读取

--- app/src/main/java/com/example/sprcontrollerkt/MainActivity.kt
+++ app/src/main/java/com/example/sprcontrollerkt/MainActivity.kt
@@ -4,22 +4,22 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.view.MotionEvent
-import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sprcontrollerkt.gamepad.GamePad
 import com.example.sprcontrollerkt.joystick.RockerView
 import com.example.sprcontrollerkt.udp.UdpClient
-import java.lang.Exception
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private val handler = Handler()
     private val client: UdpClient = UdpClient()
+    private var gamePad = GamePad()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE) //隐藏标题
@@ -30,6 +30,46 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setContentView(R.layout.activity_main)
         initViews()
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent):Boolean {
+        return if (event.source and InputDevice.SOURCE_GAMEPAD == InputDevice.SOURCE_GAMEPAD) {
+            gamePad.setKeyCode(keyCode)
+            gamePad.updateKeyValue(1)
+            true
+        } else{
+            super.onKeyUp(keyCode, event)
+        }
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        return if (event.source and InputDevice.SOURCE_GAMEPAD == InputDevice.SOURCE_GAMEPAD) {
+            gamePad.setKeyCode(keyCode)
+            gamePad.updateKeyValue(0)
+            true
+        } else{
+            super.onKeyUp(keyCode, event)
+        }
+    }
+
+    override fun onGenericMotionEvent(event: MotionEvent): Boolean {
+        // Check that the event came from a game controller
+        return if (event.source and InputDevice.SOURCE_JOYSTICK == InputDevice.SOURCE_JOYSTICK
+            && event.action == MotionEvent.ACTION_MOVE) {
+
+            // Process the movements starting from the
+            // earliest historical position in the batch
+//            (0 until event.historySize).forEach { i ->
+//                // Process the event at historical position i
+//                gamePad.processJoystickInput(event, i)
+//            }
+
+            // Process the current movement sample in the batch (position -1)
+            gamePad.processJoystickInput(event, -1)
+            true
+        } else {
+            super.onGenericMotionEvent(event)
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -44,30 +84,32 @@ class MainActivity : AppCompatActivity() {
         val button_R = findViewById<Button>(R.id.button_R)
         val set_ip = findViewById<Button>(R.id.set_ip)
         val target_ip = findViewById<EditText>(R.id.target_ip)
-        val joy_x = intArrayOf(0)
-        val joy_y = intArrayOf(0)
-        val but_A = intArrayOf(0)
-        val but_B = intArrayOf(0)
-        val but_X = intArrayOf(0)
-        val but_Y = intArrayOf(0)
-        val but_L = intArrayOf(0)
-        val but_R = intArrayOf(0)
+
+        //虚拟按键读取值
+        var joy_x = 0
+        var joy_y = 0
+        var but_A = 0
+        var but_B = 0
+        var but_X = 0
+        var but_Y = 0
+        var but_L = 0
+        var but_R = 0
 
         //开启udp消息发送线程
         Thread(object : Runnable {
             // 匿名类的Runnable接口
             @SuppressLint("SetTextI18n")
             override fun run() {
-                udpSendMsg(joy_x[0].toString() + ":" + joy_y[0] + ":" + but_A[0] + ":" + but_B[0] + ":" + but_X[0] + ":" + but_Y[0] + ":" + but_L[0] + ":" + but_R[0])
+                udpSendMsg("$joy_x:$joy_y:$but_A:$but_B:$but_X:$but_Y:$but_L:$but_R")
                 textView.text = """
-                     ${joy_x[0]}
-                     ${joy_y[0]}
-                     ${but_A[0]}
-                     ${but_B[0]}
-                     ${but_X[0]}
-                     ${but_Y[0]}
-                     ${but_L[0]}
-                     ${but_R[0]}
+                     $joy_x
+                     $joy_y
+                     $but_A
+                     $but_B
+                     $but_X
+                     $but_Y
+                     $but_L
+                     $but_R
                      """.trimIndent()
                 handler.postDelayed(this, 50)
             }
@@ -84,13 +126,13 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun value(x: Float, y: Float, regionRadius: Float) {
-                joy_x[0] = (x / regionRadius * 128 + 128).toInt()
-                joy_y[0] = (y / regionRadius * 128 + 128).toInt()
+                joy_x = (x / regionRadius * 128 + 128).toInt()
+                joy_y = (y / regionRadius * 128 + 128).toInt()
             }
 
             override fun onFinish() {
-                joy_x[0] = 0
-                joy_y[0] = 0
+                joy_x = 0
+                joy_y = 0
             }
         })
         set_ip.setOnTouchListener { v: View?, event: MotionEvent ->
@@ -101,49 +143,49 @@ class MainActivity : AppCompatActivity() {
         }
         button_A.setOnTouchListener { v: View?, event: MotionEvent ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                but_A[0] = 1
+                but_A = 1
             } else if (event.action == MotionEvent.ACTION_UP) {
-                but_A[0] = 0
+                but_A = 0
             }
             false
         }
         button_B.setOnTouchListener { v: View?, event: MotionEvent ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                but_B[0] = 1
+                but_B = 1
             } else if (event.action == MotionEvent.ACTION_UP) {
-                but_B[0] = 0
+                but_B = 0
             }
             false
         }
         button_X.setOnTouchListener { v: View?, event: MotionEvent ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                but_X[0] = 1
+                but_X = 1
             } else if (event.action == MotionEvent.ACTION_UP) {
-                but_X[0] = 0
+                but_X = 0
             }
             false
         }
         button_Y.setOnTouchListener { v: View?, event: MotionEvent ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                but_Y[0] = 1
+                but_Y = 1
             } else if (event.action == MotionEvent.ACTION_UP) {
-                but_Y[0] = 0
+                but_Y = 0
             }
             false
         }
         button_L.setOnTouchListener { v: View?, event: MotionEvent ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                but_L[0] = 1
+                but_L = 1
             } else if (event.action == MotionEvent.ACTION_UP) {
-                but_L[0] = 0
+                but_L = 0
             }
             false
         }
         button_R.setOnTouchListener { v: View?, event: MotionEvent ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                but_R[0] = 1
+                but_R = 1
             } else if (event.action == MotionEvent.ACTION_UP) {
-                but_R[0] = 0
+                but_R = 0
             }
             false
         }
---
+++ app/src/main/java/com/example/sprcontrollerkt/gamepad/GamePad.kt
@@ -0,0 +1,138 @@
+package com.example.sprcontrollerkt.gamepad
+
+import android.util.Log
+import android.view.InputDevice
+import android.view.KeyEvent
+import android.view.MotionEvent
+import kotlin.math.abs
+
+class GamePad {
+
+    private var mKeyCode = 0
+
+    private var buttonA = 0
+    private var buttonB = 0
+    private var buttonX = 0
+    private var buttonY = 0
+    private var buttonL1 = 0
+    private var buttonL3 = 0
+    private var buttonR1 = 0
+    private var buttonR3 = 0
+    private var buttonSelect = 0
+    private var buttonStart = 0
+    private var buttonUp = 0
+    private var buttonDown = 0
+    private var buttonLeft = 0
+    private var buttonRight = 0
+
+    private var axisX = 0f
+    private var axisY = 0f
+    private var axisZ = 0f
+    private var axisRZ = 0f
+    private var axisLT = 0f
+    private var axisRT = 0f
+
+
+    init {
+        Log.i("GamePad", "初始化手柄")
+    }
+
+    //读取按键信息
+    fun setKeyCode(KeyCode: Int) {
+        this.mKeyCode = KeyCode
+    }
+
+    //更新按键信息
+    fun updateKeyValue(value: Int) {
+        when (this.mKeyCode) {
+            KeyEvent.KEYCODE_BUTTON_A -> {
+                buttonA = value
+            }
+            KeyEvent.KEYCODE_BUTTON_B -> {
+                buttonB = value
+            }
+            KeyEvent.KEYCODE_BUTTON_X -> {
+                buttonX = value
+            }
+            KeyEvent.KEYCODE_BUTTON_Y -> {
+                buttonY = value
+            }
+            KeyEvent.KEYCODE_BUTTON_L1 -> {
+                buttonL1 = value
+            }
+            KeyEvent.KEYCODE_BUTTON_R1 -> {
+                buttonR1 = value
+            }
+            KeyEvent.KEYCODE_BUTTON_THUMBL -> {
+                buttonL3 = value
+            }
+            KeyEvent.KEYCODE_BUTTON_THUMBR -> {
+                buttonR3 = value
+            }
+            KeyEvent.KEYCODE_BUTTON_SELECT -> {
+                buttonSelect = value
+            }
+            KeyEvent.KEYCODE_BUTTON_START -> {
+                buttonStart = value
+            }
+            KeyEvent.KEYCODE_DPAD_UP -> {
+                buttonUp = value
+            }
+            KeyEvent.KEYCODE_DPAD_DOWN -> {
+                buttonDown = value
+            }
+            KeyEvent.KEYCODE_DPAD_LEFT -> {
+                buttonLeft = value
+            }
+            KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                buttonRight = value
+            }
+        }
+    }
+
+    private fun getCenteredAxis(
+        event: MotionEvent,
+        device: InputDevice,
+        axis: Int,
+        historyPos: Int
+    ): Float {
+        val range: InputDevice.MotionRange? = device.getMotionRange(axis, event.source)
+
+        // A joystick at rest does not always report an absolute position of
+        // (0,0). Use the getFlat() method to determine the range of values
+        // bounding the joystick axis center.
+        range?.apply {
+            val value: Float = if (historyPos < 0) {
+                event.getAxisValue(axis)
+            } else {
+                event.getHistoricalAxisValue(axis, historyPos)
+            }
+
+            // Ignore axis values that are within the 'flat' region of the
+            // joystick axis center.
+            if (abs(value) > flat) {
+                return value
+            }
+        }
+        return 0f
+    }
+
+    fun processJoystickInput(event: MotionEvent, historyPos: Int) {
+
+        val inputDevice = event.device
+
+        axisX = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_X, historyPos)
+
+        axisY = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_Y, historyPos)
+
+        axisZ = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_Z, historyPos)
+
+        axisRZ = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_RZ, historyPos)
+
+        axisLT = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_LTRIGGER, historyPos)
+
+        axisRT = getCenteredAxis(event, inputDevice, MotionEvent.AXIS_RTRIGGER, historyPos)
+
+        // Update the ship object based on the new x and y values
+    }
+}
